a934bd6a2196c4d523fbf4186c48aefe51836979
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-30 04:19:19 +0000

    primitive rock page

--- app.rb
+++ app.rb
@@ -7,3 +7,31 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else @comp_move == "scissors"
+    @outcome = "won"
+  end
+
+  erb(:rock)
+end
+
+get("/paper") do
+  "
+  You chose paper!
+  "
+end
+
+get("/scissors") do
+  "
+  You chose scissors!
+  "
+end
